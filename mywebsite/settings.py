8636491bffb330bdc433de9803c6c45e58c3742e
libelo <libelo28@gmail.com>
2013-08-20 00:50:39 +0900

    media root path 수정

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -51,7 +51,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = 'yighub/media/'
+MEDIA_ROOT = path.join(path.dirname(__file__), "../yighub/media/")
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
