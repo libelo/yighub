92391324b745202d340f2867b46c8456374eaea6
libelo <libelo28@gmail.com>
2013-11-23 22:21:51 +0900

    IE 검사로 다운로드 오류 해결 시도

--- yighub/views.py
+++ yighub/views.py
@@ -1471,8 +1471,17 @@ def download(request, file_id, file_name):
     response['Content-Type'] = filetype
     response['Content-Encoding'] = encoding
     response['Content-Length'] = os.path.getsize(path)
-    response['Content-Disposition'] = "attachment; filename*=UTF-8''%s" % urlquote(os.path.basename(path))
-    
+
+    # To inspect details for the below code, see http://greenbytes.de/tech/tc2231/
+    if u'MSIE' in request.META['HTTP_USER_AGENT']:
+        # IE does not support internationalized filename at all.
+        # It can only recognize internationalized URL, so we do the trick via routing rules.
+        filename_header = ""
+    else:
+        # For others like Firefox, we follow RFC2231 (encoding extension in HTTP headers).
+        filename_header = "attachment; filename*=UTF-8''%s" % urlquote(f.name)
+    response['Content-Disposition'] = filename_header
+
     f.hit += 1
     f.save()
     
