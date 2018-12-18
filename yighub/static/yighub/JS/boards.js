a05010d8778cddea9ab2cf763005a15434940881
WangHyungJun <whj2013123218@gmail.com>
2018-12-19 01:57:55 +0200

    Search Design Revised

---
+++ yighub/static/yighub/JS/boards.js
@@ -0,0 +1,24 @@
+var sub_one=$(".sub_depth1_drop");
+var vanilla_one=document.querySelector('.search');
+
+// $("#current").click(function (){
+//    if($(".search").css("margin-top")!="70px"){
+//     $(".search").css("margin-top", "70px");
+//    }else{
+//     $(".search").css("margin-top", "20px");
+//    }
+// });
+
+$("#current_two").click(function (){
+   if($(".search").css("margin-top")!="200px"){
+    $(".search").css("margin-top", "200px");
+   }else{
+    $(".search").css("margin-top", "20px");
+   }
+});
+//
+// function adjust() {
+//     if (window.matchMedia('(max-width:500px)') && vanilla_one.style.marginTop != "200px"){
+//         vanilla_one.style.marginTop="200px";
+//     }
+// }
