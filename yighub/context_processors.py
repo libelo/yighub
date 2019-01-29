79469d7f3df436e8f62616bdab89b270b606614e
WangHyungJun <whj2013123218@gmail.com>
2019-01-29 14:20:17 +0200

    Index Error Fixed

--- yighub/context_processors.py
+++ yighub/context_processors.py
@@ -6,7 +6,7 @@ def users(request):
     active_fund=PublicBoard.objects.filter(active_fund=True)
 
     if len(active_fund)==0:
-        active_fund=PublicBoard.objects.filter(id=93)
+        active_fund=PublicBoard.objects.filter(id=98)
         context={'rep_fund': active_fund[0]}
     else:
         context={'rep_fund': active_fund[0]}
